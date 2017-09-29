cf3ac0394ca8b77c178f537a4b80c5a68e69c095
Jonathan Topf <jonathantopf@gmail.com>
2017-09-29 11:26:34 +0100

    Added a scene about simple guided physical like movement.

---
+++ AnimatedExamples_unity/Assets/Scripts/SimpleGuidedPhysicalLikeMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleGuidedPhysicalLikeMovement : MonoBehaviour 
+{
+	public Transform target; // Pink ball;
+	public Transform follower; // White ball;
+
+	public float acceleration = 5f;
+	public float attenuation = 0.9f;
+
+	Vector3 _velocity;
+
+	void Update ()
+	{
+		Vector3 towardTarget = target.position - follower.position;
+		_velocity += acceleration * towardTarget;
+		_velocity *= attenuation;
+		follower.position += _velocity * Time.deltaTime;
+	}
+
+	// jonathantopf.com
+}
+
+
+
+
